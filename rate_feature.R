39fc0fb443b9dcdb5588a9496f1f706a8896fa35
Rupy <rupyapps@gmail.com>
2014-06-19 01:20:00 +0900

    構成要素と品詞を区別するコメントの追加

--- rate_feature.R
+++ rate_feature.R
@@ -1,6 +1,10 @@
 library(RMeCab)
 library(stringr)
 
+#
+# 構成要素に関する関数
+#
+
 # ひらがなの割合を数える関数
 hiragana.rate <- function(str){
   return(str_count(str,"[ぁ-ん]")/nchar(str))
@@ -21,37 +25,18 @@ alphabet.rate <- function(str){
 number.rate <- function(str){
   return(str_count(str,"[0-9]")/nchar(str))
 }
-
+# 記号を数える関数
 simbol.rate <- function(str){
   punct <- str_extract_all(str, "[[:punct:]]")[[1]]
   return(length(punct)/nchar(str))
 }
-
+# 空白を数える関数
 blank.rate <- function(str){
   
   blank<- str_extract_all(str, "[[:blank:]]")[[1]]
   return(length(blank)/nchar(str))
 }
-
-create.rate.mat <-function(strvec){
-  rate.mat <- sapply(strvec,rate.vector)
-  colnames(rate.mat) <- NULL
-  rate.mat[is.na(rate.mat)] <- 0
-  rownames(rate.mat)  <- struct.pos.simbol
-  rate.mat
-}
-
-
-rate.vector <- function(str){
-  rate.vec <- c(
-    structure.rate.vector(str),
-    pos.rate.vector(str)
-    #rate_def_count(str),
-    #check_easy(str)
-  )
-  rate.vec
-}
-
+# 構成要素の含有率を基に特徴ベクトルを作成
 structure.rate.vector <- function(str){
   rate.vec <- c(
     hiragana.rate(str),
@@ -65,28 +50,57 @@ structure.rate.vector <- function(str){
   rate.vec
 }
 
-pos.rate.vector <- function(str){
-  rate.vec <- c(
-    rate.type(str,type="名詞"),
-    rate.type(str,type="動詞"),
-    rate.type(str,type="形容詞"),
-    rate.type(str,type="副詞"),
-    rate.type(str,type="助詞"),
-    rate.type(str,type="接続詞"),
-    rate.type(str,type="助動詞"),
-    rate.type(str,type="連体詞"),
-    rate.type(str,type="感動詞")
-  )
-  rate.vec  
-}
+#
+# 品詞に関する関数
+#
 
 # 品詞を数える関数
-rate.type <- function(str,type){
+rate.pos <- function(str,type){
   if(nchar(str)==0){return(0)}
   res <- RMeCabC(str)
   res <- unlist(res)
   res2 <- res[names(res) == type]
   rate <- length(res2)/length(res)
   return(rate)
+}
+
+# 品詞の含有率を基に特徴ベクトルを作成
+pos.rate.vector <- function(str){
+  rate.vec <- c(
+    rate.pos(str,type="名詞"),
+    rate.pos(str,type="動詞"),
+    rate.pos(str,type="形容詞"),
+    rate.pos(str,type="副詞"),
+    rate.pos(str,type="助詞"),
+    rate.pos(str,type="接続詞"),
+    rate.pos(str,type="助動詞"),
+    rate.pos(str,type="連体詞"),
+    rate.pos(str,type="感動詞")
+  )
+  rate.vec  
+}
+
+#
+# 構成要素と品詞に関する関数
+#
+
+# 品詞の含有率を基に特徴ベクトルを作成
+rate.vector <- function(str){
+  rate.vec <- c(
+    structure.rate.vector(str),
+    pos.rate.vector(str)
+    #rate_def_count(str),
+    #check_easy(str)
+  )
+  rate.vec
+}
+
+# 行列の作成
+create.rate.mat <-function(strvec){
+  rate.mat <- sapply(strvec,rate.vector)
+  colnames(rate.mat) <- NULL
+  rate.mat[is.na(rate.mat)] <- 0
+  rownames(rate.mat)  <- struct.pos.simbol
+  rate.mat
 }
 
